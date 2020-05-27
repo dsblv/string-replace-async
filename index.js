6745ef97e3b2d21abaf6ba8b0daf6c6bae6f72b2
Dmitrii Sobolev <disobolev@icloud.com>
2020-05-27 14:27:17 +0300

    Thought of another way to do it

--- index.js
+++ index.js
@@ -1,98 +1,32 @@
-'use strict';
-var escapeStringRegexp = require('escape-string-regexp');
-
-function matchAll(str, re) {
-	var matches = [];
-	var res = re.exec(str);
-
-	while (res) {
-		matches.push(res);
-
-		if (!re.global) {
-			break;
-		}
-
-		res = re.exec(str);
-	}
-
-	return matches;
-}
-
-function replaceAll(str, matches) {
-	return matches.reduceRight(function (res, match) {
-		var prefix = res.slice(0, match.index);
-		var postfix = res.slice(match.index + match[0].length);
-
-		return prefix + match.replacement + postfix;
-	}, str);
-}
-
-function assignReplacement(match, replacer) {
-	var args = match.concat([match.index, match.input]);
-
-	return replacer.apply(null, args).then(function (res) {
-		return {
-			0: match[0],
-			index: match.index,
-			replacement: res
-		};
-	});
-}
-
-function sequence(matches, replacer) {
-	var initialResult = Promise.resolve([]);
-
-	return matches.reduce(function (prev, match) {
-		return prev.then(function (ret) {
-			return assignReplacement(match, replacer).then(function (res) {
-				return ret.concat([res]);
-			});
-		});
-	}, initialResult);
-}
-
-function concurrency(matches, replacer) {
-	var promises = matches.map(function (match) {
-		return assignReplacement(match, replacer);
-	});
-
-	return Promise.all(promises);
-}
-
-function processString(str, re, replacer, seq) {
-	if (typeof replacer === 'string') {
-		return str.replace(re, replacer);
-	}
-
-	if (typeof re === 'string') {
-		re = new RegExp(escapeStringRegexp(re));
-	}
-
-	var matches = matchAll(str, re);
-	var processor = seq ? sequence : concurrency;
-
-	return processor(matches, replacer).then(function (matches) {
-		return replaceAll(str, matches);
-	});
-}
-
-function fn(str, re, replacer, seq) {
-	if (re instanceof RegExp) {
-		re.lastIndex = 0;
-	}
-	try {
-		return Promise.resolve(processString(str, re, replacer, seq));
-	} catch (e) {
-		return Promise.reject(e);
-	}
-}
-
-function stringReplaceAsync(str, re, replacer) {
-	return fn(str, re, replacer, false);
-}
-
-stringReplaceAsync.seq = function (str, re, replacer) {
-	return fn(str, re, replacer, true);
+module.exports = function stringReplaceAsync(
+  string,
+  searchValue,
+  replaceValue
+) {
+  try {
+    if (typeof replaceValue === "function") {
+      // Step 1: Call native `replace` one time to acquire arguments for
+      // `replaceValue` function
+      // Step 2: Collect all return values in an array
+      // Step 3: Run `Promise.all` on collected values to resolve them
+      // Step 4: Call native `replace` the second time, replacing substrings
+      // with resolved values in order of occurance!
+      var promises = [];
+      String.prototype.replace.call(string, searchValue, function () {
+        promises.push(replaceValue.apply(undefined, arguments));
+        return "";
+      });
+      return Promise.all(promises).then(function (values) {
+        return String.prototype.replace.call(string, searchValue, function () {
+          return values.shift();
+        });
+      });
+    } else {
+      return Promise.resolve(
+        String.prototype.replace.call(string, searchValue, replaceValue)
+      );
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
-
-module.exports = stringReplaceAsync;
